f7013c8d2d3af01cc1842d6fdc83ab7375286a6b
Zoe Keller <keller.zoe98@gmail.com>
2020-06-11 00:14:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,8 @@
 class Song 
-  @@song_count=0 
+  @@count=0 
   
   def initialize (name, artist, genre)
-    @@song_count+=1 
+    @@count+=1 
     @name=name 
     @artist=artist 
     @genre=genre
