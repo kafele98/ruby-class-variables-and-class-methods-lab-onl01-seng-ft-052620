428544043dfbdc3d4fb78298d6897392845a68ea
Zoe Keller <keller.zoe98@gmail.com>
2020-06-11 00:18:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,23 +12,6 @@ class Song
     @count
   end 
   
-  def self.artist_count
-    @@artists.inject(Hash.new(0)) { |total, i| total[i] += 1 ;total}
-  end
-  
-  def self.artists
-    @@artists.uniq
-  end
-  
-  def initialize(name, artist, genre)
-    @name = name
-    @artist = artist
-    @@artists << artist
-    @genre = genre
-    @@genres << genre
-    @@count += 1
-  end
-  
   attr_accessor :name 
   
   attr_accessor :artist 
