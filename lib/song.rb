c7df716c5503922d0bf7833471f37021bbd08c92
Zoe Keller <keller.zoe98@gmail.com>
2020-06-11 00:16:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,13 +3,13 @@ class Song
   
   def initialize (name, artist, genre)
     @@count+=1 
-    @@name=name 
-    @@artist=artist 
-    @@genre=genre
+    @name=name 
+    @artist=artist 
+    @genre=genre
   end 
   
   def self.count 
-    @@count
+    @count
   end 
   
   def artists 
